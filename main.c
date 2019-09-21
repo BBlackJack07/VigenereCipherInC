ddad3d0f7451e8f7be2ae57a9e346c1c01bd7807
bblackjack07 <bastien.brunel.bb@gmail.com>
2019-09-21 18:21:52 +0200

    add constant MAX_MSG_SIZE

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "vigenere.h"
+#define MAX_MSG_SIZE 100000
 
 char readFile(char buffer[], char path_to_file[]);
 void showHelp(void);
@@ -9,24 +10,24 @@ int main(int argc, char* argv[])
     {
         if (!strcmp("--crypt", argv[1]))
         {
-            char encrypted_msg[100000] = "";
+            char encrypted_msg[MAX_MSG_SIZE] = "";
             vencrypt(argv[2], argv[3], encrypted_msg);
             printf("%s\n", encrypted_msg);
             return EXIT_SUCCESS;
         }
         else if (!strcmp("--decrypt", argv[1]))
         {
-            char decrypted_msg[100000] = "";
+            char decrypted_msg[MAX_MSG_SIZE] = "";
             vdecrypt(argv[2], argv[3], decrypted_msg);
             printf("%s\n", decrypted_msg);
             return EXIT_SUCCESS;
         }
         else if (!strcmp("--crypt-file", argv[1]))
         {
-            char buffer[100000] = "";
+            char buffer[MAX_MSG_SIZE] = "";
             if (readFile(buffer, argv[2]))
             {
-                char encrypted_msg[100000] = "";
+                char encrypted_msg[MAX_MSG_SIZE] = "";
                 vencrypt(buffer, argv[3], encrypted_msg);
                 printf("%s\n", encrypted_msg);
                 return EXIT_SUCCESS;
@@ -41,10 +42,10 @@ int main(int argc, char* argv[])
         }
         else if (!strcmp("--decrypt-file", argv[1]))
         {
-            char buffer[100000] = "";
+            char buffer[MAX_MSG_SIZE] = "";
             if (readFile(buffer, argv[2]))
             {
-                char decrypted_msg[100000] = "";
+                char decrypted_msg[MAX_MSG_SIZE] = "";
                 vdecrypt(buffer, argv[3], decrypted_msg);
                 printf("%s\n", decrypted_msg);
                 return EXIT_SUCCESS;
